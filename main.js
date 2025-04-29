9a8adce486b25e54a81ce256308acaef4a839ffa
alejandro0721-b <luisalejo121@gmail.com>
2025-04-29 11:55:27 -0500

    fetch pokemon y display pokemon

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
 const pokemonList = document.getElementById("pokemonList") //const (constante) no permite cambiar la variable
 
+async function fetchPokemonData(pokemonId){
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`) 
+        //"https://pokeapi.co/api/v2/pokemon/"+pokemonId
+        const data = await response.json() //json debe ir con await, es un proceso asincrono
+        console.log(data)
+        return data    
+    } catch (error) {
+        console.error(error)
+        return false
+    }
+}
+
+function displayPokemon(pokemon){
+    const pokemonCard = document.createElement("div")
+    pokemonCard.classList.add("pokemon-card")
+    console.log(pokemon.sprites.front_shiny)
+    pokemonCard.innerHTML = `
+        <img src="${pokemon.sprites.front_shiny}"alt="Iamgen de ${pokemon.name}">
+        <h3>${pokemon.name}</h3>
+        <h2>${pokemon.id}</h2>
+    `
+    pokemonList.appendChild(pokemonCard)
+}
+
+async function loadPokedex(){
+    const pokemon = await fetchPokemonData(4)
+    console.log(pokemon)
+    displayPokemon(pokemon)
+}   
+loadPokedex()
